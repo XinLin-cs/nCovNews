e1c82553dcc89876321d4ce7ecc1c413194a21a0
Echohat <fzulinxin@163.com>
2020-04-22 10:21:02 +0800

    2020-04-22 findbydate

--- nCovNews/nCovNews/__init__.py
+++ nCovNews/nCovNews/__init__.py
@@ -14,4 +14,7 @@ import pandas as pd
 from nCovNews import database as db
 data_all = db.get_df() # 从数据库内读取数据表
 date = datetime.date.today()
-data = db.findbydate( data_all , date ) # 据日期获得筛选后的数据表
+db.findbydate( data_all , date , 'All' , 'None' , 'None' ) 
+db.findbydate( data_all , 'All' , '中国' , 'None' , 'None' ) 
+db.findbydate( data_all , 'All' , '美国' , 'None' , 'None' ) 
+db.findbydate( data_all , 'All' , 'All' , '湖北省' , 'None' )
--- nCovNews/nCovNews/database.py
+++ nCovNews/nCovNews/database.py
@@ -5,28 +5,21 @@ import requests
 import pandas as pd
 import numpy as np
 
-# Template
-"""
-data_all = get_df() # 从数据库内读取数据表
-date = datetime.date.today()
-findbydate( data_all , date ) # 据日期获得筛选后的数据表
-
-"""
-
 def update_all():
     # 从服务器获取数据
     url = 'http://raw.githubusercontent.com/canghailan/Wuhan-2019-nCoV/master/Wuhan-2019-nCoV.json'
     try:
         data = requests.get(url).json()
+        # json获取数据帧
+        data_df = pd.DataFrame(data)
+        # 保存至数据库
+        data_df.to_csv('nCovNews/data/data_all.csv' , encoding="utf_8_sig")
     except BaseException as err:
+        print(err)
         return 1 # 错误代码1，请求失败
     except:
         return 2 # 错误代码2，未知异常
 
-    # json获取数据帧
-    data_df = pd.DataFrame(data)
-    # 保存至数据库
-    data_df.to_csv('nCovNews/data/data_all.csv' , encoding="utf_8_sig")
     return 0
 
 def get_df():
@@ -36,14 +29,41 @@ def get_df():
     data = pd.read_csv('nCovNews/data/data_all.csv')
     return data
 
-def findbydate(data_all , date):
-    # 日期转字符串
-    date = date.strftime('%Y-%m-%d')
+def findbydate(data_all , date='All' , country='All' , province='All', city='All'):
     # 筛选数据
-    data = data_all[data_all['date'] == date]
-    # 显示前100降序排序
-    data_show = data.nlargest( 100 , 'confirmed' )
-    # 导出数据
-    data_show.to_csv('nCovNews/data/show.csv' , encoding="utf_8_sig")
+    msg = ""
+    data = data_all.fillna('None')
 
+    if (date != 'All'):
+        # 日期转字符串
+        date = date.strftime('%Y-%m-%d')
+        data = data[data['date'] == date]
+        msg += date 
+    if (country != 'All'):
+        data = data[data['country'] == country]
+        if (country != 'None'):
+            msg += country 
+    if (province != 'All'):
+        data = data[data['province'] == province]
+        if (province != 'None'):
+            msg += province
+    if (city != 'All'):
+        data = data[data['city'] == city]
+        if (city != 'None'):
+            msg += city
+    # 显示前100降序排序并导出
+    if ( msg!='' ):
+        data_show = data.nlargest( 100 , 'confirmed' )
+        data_show.to_csv('nCovNews/data/%s.csv'%msg , encoding="utf_8_sig")
     return data
+
+'''
+# Template
+data_all = get_df() # 从数据库内读取数据表
+date = datetime.date.today()
+# 据日期获得筛选后的数据表
+findbydate( data_all , date , 'All' , 'None' , 'None' ) 
+findbydate( data_all , 'All' , '中国' , 'None' , 'None' ) 
+findbydate( data_all , 'All' , '美国' , 'None' , 'None' ) 
+findbydate( data_all , 'All' , 'All' , '湖北省' , 'None' )
+'''
