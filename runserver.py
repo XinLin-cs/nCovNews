29e3664dc07115ed832efe4219087e194733e927
Echohat <fzulinxin@163.com>
2020-04-27 00:26:39 +0800

    add update time

--- runserver.py
+++ runserver.py
@@ -8,7 +8,7 @@ from nCovNews import database as db
 
 if __name__ == '__main__':
     # 实时数据更新
-    db.auto_update( 60 )
+    db.auto_update( 60*60*4 )
     # 启动服务器
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
